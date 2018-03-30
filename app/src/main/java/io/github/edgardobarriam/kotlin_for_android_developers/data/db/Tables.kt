e8e7cde021681599c972e00bfbae96e6c41bf52e
Edgardo Barria Melian <edgardo.barriam@gmail.com>
2018-03-30 18:56:48 -0300

    Chapter 17: Creating an SQLiteOpenHelper

---
+++ app/src/main/java/io/github/edgardobarriam/kotlin_for_android_developers/data/db/Tables.kt
@@ -0,0 +1,19 @@
+package io.github.edgardobarriam.kotlin_for_android_developers.data.db
+
+object CityForecastTable {
+    val NAME = "CityForecast"
+    val ID = "_id"
+    val CITY = "city"
+    val COUNTRY = "country"
+}
+
+object DayForecastTable {
+    val NAME = "DayForecast"
+    val ID = "_id"
+    val DATE = "date"
+    val DESCRIPTION = "description"
+    val HIGH = "high"
+    val LOW = "low"
+    val ICON_URL = "iconUrl"
+    val CITY_ID = "cityId"
+}
